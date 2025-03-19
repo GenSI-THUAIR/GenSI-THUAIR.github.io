d8afc74c893358b5ef5222b3146e35018a590308
Hugh <hugh.lpd@gmail.com>
2025-03-19 15:15:45 +0800

    feat: contact usable

--- libs/custom/js/contact.js
+++ libs/custom/js/contact.js
@@ -1,2 +1,48 @@
 /* jshint browser: true */
 /* jshint indent: 2 */
+const host = "127.0.0.1";
+const port = "5002";
+const endpoint = "http://" + host + ":" + port;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+function contact(fname, lname, email, message) {
+  const post = "/contact";
+  var data = {
+    "fname": fname,
+    "lname": lname,
+    "email": email,
+    "message": message
+  }
+  let request = fetch(endpoint + post, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Origin": "http://61.241.63.126",
+    },
+    body: JSON.stringify(data)
+  })
+  request.then(response => {
+    if (response.ok) {
+      alert("Thank you for your message!");
+    } else {
+      alert("Error sending message. Please try again.");
+    }
+  })
+
+}
+
+function submitContact() {
+  const fname = document.getElementById("contact-fname").value;
+  const lname = document.getElementById("contact-lname").value;
+  const email = document.getElementById("contact-email").value;
+  const message = document.getElementById("contact-message").value;
+  if (emailRegex.test(email) == false) {
+    alert("Please enter a valid email address.");
+    return;
+  }
+  if (message.length == 0) {
+    alert("Empty message is not allowed.");
+    return;
+  }
+  contact(fname, lname, email, message);
+}
